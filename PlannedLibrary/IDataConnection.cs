8b60d1adb766e1b231356e3452f5a1e7d37a3c24
hamidmaz <hamid_ma1367@yahoo.com>
2019-04-01 09:24:42 +0200

    GlobalConfig and IF for creat prize added
    They are blank now

---
+++ PlannedLibrary/IDataConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannedLibrary
+{
+    public interface IDataConnection
+    {
+        /// <summary>
+        /// It save the input prize and pass it with an Id populated for it
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Prize CreatPrize(Prize model);
+    }
+}
